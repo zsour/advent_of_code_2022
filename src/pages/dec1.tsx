021e218d1597a5c14c13162dd4569e8031392f89
Daniel Karlsson <daniel.karlsson36@outlook.com>
2022-12-02 07:53:24 +0100

    File input.

--- src/pages/dec1.tsx
+++ src/pages/dec1.tsx
@@ -56,8 +56,39 @@ export default function Dec1() {
 
             <div className="input-area">
                 <div className="input-header">
-                    <p className="input-header-text">Puzzle input:</p>
+                    <p className="input-header-text">Paste puzzle input or import input file:</p>
                 </div>
+                <label htmlFor="files" className="chooseFile">
+                    <p>Select input file</p>
+                </label>
+                <input  style={{visibility: "hidden"}} type="file" id="files" onChange={(e) => {
+                        e.preventDefault();
+                        const reader = new FileReader();
+                        
+                        console.log(reader);
+                        
+                        if(e.target && e.target.files){
+                          
+                            
+                            reader.onload = async (e) => { 
+                                if(e.target){
+                                    const text = (e.target.result);
+                                    setInput(String(text));
+                                }
+                            };
+
+                            if(e.target.files[0] && e.target.files[0].type){
+                                if(e.target.files[0].type !== "text/plain"){
+                                    return;
+                                }
+
+                                reader.readAsText(e.target.files[0]);
+                            }
+
+                        
+                           
+                        }
+                }}/>
                 <textarea name="" value={input} className="input-text-area" onChange={(e) => {
                     setInput(e.target.value);
                 }} cols={30} rows={10} placeholder="Paste input here..."></textarea>
