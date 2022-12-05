cf5b412e0c932d7a1a79de8231fb992a5bbcfa8c
z_sour <daniel.karlsson36@outlook.com>
2022-12-05 07:23:40 +0100

    Dec 5 start.

--- src/pages/dec5.tsx
+++ src/pages/dec5.tsx
@@ -8,8 +8,37 @@ function Dec5(){
     const [puzzleTwoResult, setPuzzleResultTwo] = useState(0);
     const [timer, setTimer] = useState("0 ms.");
 
+    let stacks: string[][] = [
+        ["Z", "T", "F", "R", "W", "J", "G"],
+        ["G", "W", "M"],
+        ["J", "N", "H", "G"],
+        ["J", "R", "C", "N", "W"],
+        ["W", "F", "S", "B", "G", "Q", "V", "M"],
+        ["S", "R", "T", "D", "V", "W", "C"],
+        ["H", "B", "N", "C", "D", "Z", "G", "V"],
+        ["S", "J", "N", "M", "G", "C"],
+        ["G", "P", "N", "W", "C", "J", "D", "L"]
+    ];
 
 
+    function moveCreates(arr: string[][], numberOfCrates: number, from: number, to: number){
+        let tmp = arr;
+        if(numberOfCrates > tmp[from - 1].length){
+            numberOfCrates = tmp[from - 1].length;
+        }
+
+        let newArr: string[] = [];
+        for(let i = 0; i < numberOfCrates; i++){   
+            newArr[i] = tmp[from - 1][tmp[from - 1].length - 1];
+            tmp[from - 1].pop();
+        }
+
+        for(let i = 0; i < newArr.length; i++){
+            tmp[to - 1].push(newArr[i]);
+        }
+
+        return tmp;
+    }
 
     useEffect(() => {
         if(input.length === 0){
@@ -23,9 +52,24 @@ function Dec5(){
             tmp.pop();
         }
 
-        // Insert code here...
+        let newArr = tmp.map((str) => {
+            let newStr = str.split("");
+            return newStr.filter(Number);
+        });
 
+        for(let i = 0; i < newArr.length; i++){
+            if(newArr[i][0] && newArr[i][1] && newArr[i][2]){   
+                stacks = moveCreates(stacks, +newArr[i][0], +newArr[i][1], +newArr[i][2]);
+            }
+        }
+        
+        let returnString = [];
+        for(let i = 0; i < stacks.length; i++){
+            returnString.push(stacks[i][stacks[i].length - 1]);
+        }
 
+       
+        console.log(stacks, String(returnString));
         let endTimer = performance.now();
         setTimer(`${(endTimer - startTimer).toFixed(1)} ms.`);
        
