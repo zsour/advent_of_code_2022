b6c9647358bee53bac43d4df564a17278ac4c86c
z_sour <daniel.karlsson36@outlook.com>
2022-12-11 13:26:18 +0100

    Dec 11, part 1.

--- src/pages/dec11.tsx
+++ src/pages/dec11.tsx
@@ -2,13 +2,38 @@ import {useState, useEffect} from 'react';
 import MainCard from '../components/MainCard';
 import InputArea from '../components/InputArea';
 
+interface Operation{
+    operator: string;
+    numberOne: string;
+    numberTwo: string;
+}
+
+interface Monkey{
+    inspectedItems: number;
+    items: number[];
+    opereration: Operation;
+    conditionalDivision: number;
+    trueThrowTo: number;
+    falseThrowTo: number;
+}
+
+function doOpertion(op: Operation, old: number){
+    let numberOne = (op.numberOne === 'old') ? old : +op.numberOne;
+    let numberTwo = (op.numberTwo === 'old') ? old : +op.numberTwo;
+    
+    switch(op.operator){
+        case "+": return numberOne + numberTwo;
+        case "*": return numberOne * numberTwo;
+        default: return 0;
+    }
+}
+
 function Dec11(){
     const [input, setInput] = useState("");
     const [puzzleOneResult, setPuzzleOneResult] = useState(0);
     const [puzzleTwoResult, setPuzzleTwoResult] = useState(0);
     const [timerOne, setTimerOne] = useState("0 ms.");
     const [timerTwo, setTimerTwo] = useState("0 ms.");
-
       
     useEffect(() => {
         if(input.length === 0){
@@ -22,7 +47,91 @@ function Dec11(){
             tmp.pop();
         }
 
+        let monkeyList: Monkey[] = [];
+
+        for(let i = 0; i < tmp.length; i+=7){
+            let startingItems = tmp[i+1];
+            let startingItemsStringArr = startingItems.split(" ");
+            let startingItemsArr = startingItemsStringArr.map((str: string) => {
+                let tmpString = str.replace(',', '');
+                return +tmpString;
+            });
+            startingItemsArr.splice(0, 4);
+            
+
+            let operation = tmp[i+2];
+            let operationStringArr = operation.split(" ");
+            operationStringArr.splice(0, operationStringArr.length - 3);
+
+            let conditionalDivisionArr = tmp[i+3].split(" ");
+            conditionalDivisionArr.splice(0, conditionalDivisionArr.length - 1);
+            let conditionalDivision = +conditionalDivisionArr[0];
+            
+
+            let trueThrowToArr = tmp[i+4].split(" ");
+            trueThrowToArr.splice(0, trueThrowToArr.length - 1);
+            let falseThrowToArr = tmp[i+5].split(" ");
+            falseThrowToArr.splice(0, falseThrowToArr.length - 1);
+
+            let trueThrowTo = +trueThrowToArr[0];
+            let falseThrowTo = +falseThrowToArr[0];
+
+            let monkey: Monkey = {
+                inspectedItems: 0,
+
+                items: startingItemsArr, 
+                
+                opereration: {
+                    operator: operationStringArr[1],
+                    numberOne: operationStringArr[0],
+                    numberTwo: operationStringArr[2]
+                }, 
+
+                conditionalDivision,
+                trueThrowTo,
+                falseThrowTo
+            };
+
+            monkeyList.push(monkey);
+        }
+
+        for(let rounds = 0; rounds < 10000; rounds++){
+            for(let i = 0; i < monkeyList.length; i++){
+                if(monkeyList[i].items.length !== 0){
+                    for(let j = 0; j < monkeyList[i].items.length; j++){
+                        let oldWorryLevel = monkeyList[i].items[j];
+                        let newWorryLevel = doOpertion(monkeyList[i].opereration, oldWorryLevel);
         
+                        
+                        if(newWorryLevel % monkeyList[i].conditionalDivision === 0){
+                            monkeyList[monkeyList[i].trueThrowTo].items.push(newWorryLevel);
+                        }else{
+                            monkeyList[monkeyList[i].falseThrowTo].items.push(newWorryLevel);
+                        }
+                    }
+        
+                    monkeyList[i].inspectedItems += monkeyList[i].items.length;
+                    monkeyList[i].items = [];
+                };
+            }
+        }
+
+        console.log(monkeyList);
+        
+
+        let inspectedItemsList: number[] = [];
+        for(let i = 0; i < monkeyList.length; i++){
+            inspectedItemsList.push(monkeyList[i].inspectedItems);
+        }
+
+        inspectedItemsList = inspectedItemsList.sort((a, b) => {
+            return a - b;
+        });
+
+        console.log(inspectedItemsList);
+        
+        setPuzzleOneResult(inspectedItemsList[inspectedItemsList.length - 2] * inspectedItemsList[inspectedItemsList.length - 1]);
+    
         setTimerTwo(`${(performance.now() - startTimer).toFixed(1)} ms.`);
     }, [input]);
 
@@ -34,7 +143,7 @@ function Dec11(){
             <div className="result-area">
                 <div className="result">
                     <p className="result-text">
-                        Result 1: {String(puzzleOneResult)}.
+                        Monkey business after 20 rounds: {String(puzzleOneResult)}.
                     </p>
 
                     <p className="result-text">
