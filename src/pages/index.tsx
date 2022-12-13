47455f4db4d878cfc981420c95b31967adb1ab70
Daniel Karlsson <daniel.karlsson36@outlook.com>
2022-12-13 15:04:26 +0100

    Checkmarks.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,55 +6,55 @@ export default function Home() {
         <MainCard title="Advent of Code 2022">
             <ul className='puzzle-list'>
               <Link href="/dec1">
-                <li>Dec 1: Calorie Counting.</li>
+                <li>✅Dec 1: Calorie Counting.</li>
               </Link>
 
               <Link href="/dec2">
-                <li>Day 2: Rock Paper Scissors.</li>
+                <li>✅Day 2: Rock Paper Scissors.</li>
               </Link>
 
               <Link href="/dec3">
-                <li>Day 3: Rucksack Reorganization.</li>
+                <li>✅Day 3: Rucksack Reorganization.</li>
               </Link>
 
               <Link href="/dec4">
-                <li>Day 4: Camp Cleanup.</li>
+                <li>✅Day 4: Camp Cleanup.</li>
               </Link>
 
               <Link href="/dec5">
-                <li>Day 5: Supply Stacks.</li>
+                <li>✅Day 5: Supply Stacks.</li>
               </Link>
 
               <Link href="/dec6">
-                  <li>Day 6: Tuning Trouble.</li>
+                  <li>✅Day 6: Tuning Trouble.</li>
               </Link>
 
               <Link href="/dec7">
-                <li>Day 7: No Space Left On Device.</li>
+                <li>✅Day 7: No Space Left On Device.</li>
               </Link>
 
               <Link href="/dec8">
-                <li>Day 8: Treetop Tree House.</li>
+                <li>✅Day 8: Treetop Tree House.</li>
               </Link>
 
               <Link href="/dec9">
-                <li>Day 9: Rope Bridge.</li>
+                <li>✅Day 9: Rope Bridge.</li>
               </Link>
 
               <Link href="/dec10">
-                <li>Day 10: Cathode-Ray Tube.</li>              
+                <li>✅Day 10: Cathode-Ray Tube.</li>              
               </Link>
 
               <Link href="/dec11">
-                <li>Day 11: Monkey in the Middle.</li>              
+                <li>Day 11: Monkey in the Middle. (Part 2: Work in progress...)</li>              
               </Link>
 
               <Link href="/dec12">
-                <li>Day 12: Hill Climbing Algorithm.</li>              
+                <li>Day 12: Hill Climbing Algorithm. (Work in progress...)</li>              
               </Link>
 
               <Link href="/dec13">
-                <li>Day 13: Distress Signal.</li>              
+                <li>✅Day 13: Distress Signal.</li>              
               </Link>
             </ul>
         </MainCard>
